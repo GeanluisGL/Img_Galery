effffeda8f5702ac59ce8b029a8629240d450cae
GeanluisGL <94375631+GeanluisGL@users.noreply.github.com>
2022-03-26 21:05:33 -0400

    Official

---
+++ script.js
@@ -0,0 +1,14 @@
+const fullb= document.getElementById("boxf")
+const fulli= document.getElementById("full")
+
+function oa(refer){
+    fullb.style.display = "flex";
+    fulli.src = refer;
+}
+
+function off(){
+    fullb.style.display = "none";
+
+}
+
+
